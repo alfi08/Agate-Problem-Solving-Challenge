a611dd08ef48cf3a6c96c68af81f17d81942bc92
alfi sahri <alfisahri33@gmail.com>
2021-10-12 19:57:36 +0700

    solved

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+  private Rigidbody2D Rb;
+  private Vector2 direction;
   public float ms;
-  Rigidbody2D Rb;
   // Start is called before the first frame update
   void Start()
   {
     Rb = GetComponent<Rigidbody2D>();
-    Rb.velocity = new Vector2(ms * 3, ms * 3);
   }
 
   // Update is called once per frame
   void Update()
   {
+    float hor = Input.GetAxisRaw("Horizontal");
+    float ver = Input.GetAxisRaw("Vertical");
 
+    Rb.velocity = new Vector2(ms * hor, ms * ver);
+
+    if (Input.GetMouseButton(1))
+    {
+      Vector2 mouse = Input.mousePosition;
+      Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+      Rb.velocity = new Vector2(ms * castPoint.origin.x, ms * castPoint.origin.y);
+    }
   }
 }
